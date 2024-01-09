6bcbc80e98821bfaef11fcbf83e0b4cfff1b68d8
Romain Planchon <romain.planchon@telecom-paris.fr>
2024-01-09 10:57:19 +0100

    THIS VERSION ALLOW USER TO MAKE TRANSACTION WITH LYDIA API ON THE TEST ENVIRONNEMENT

--- sitweb/src/shop/urls.py
+++ sitweb/src/shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index,shop
+from .views import index,shop,order_capsule
 
 
 urlpatterns = [
     path('',shop, name = 'boutique'),
+    path('order_capsule', order_capsule,name='order_capsule' )
 ]
 
--- sitweb/src/shop/views.py
+++ sitweb/src/shop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Capsule
+from django.http import JsonResponse,HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt,csrf_protect
+import requests
 
-DEBUG = True
+DEBUG = False
 
 # Create your views here.
 
@@ -22,3 +25,41 @@ def shop(request):
     
     return render(request, 'shop/index.html', context)
 
+@csrf_protect
+def order_capsule(request):
+    DEBUG = False
+
+    if request.method == 'POST':
+        
+        vendor_token = '632b3978223b2045410032'
+        url = "https://homologation.lydia-app.com/api/request/do.json"
+
+        capsule_amount = request.POST.get('capsule_prix')  # Assuming you send the capsule_id in the POST request
+        account_num = request.POST.get('account_num')
+
+        # Modify the data dictionary with dynamic values based on the capsule_id or any other relevant information
+        data = {
+            "amount": int(capsule_amount)/100,
+            "payment_method": "auto",
+            "vendor_token": vendor_token,
+            "recipient": account_num,
+            "currency": "EUR",
+            "type": "phone",
+            
+        }
+
+        response = requests.post(url, data=data)
+        if response.status_code == 200 : 
+            
+            transaction_data = response.json()
+            redirect_url = transaction_data['mobile_url']
+            
+            if DEBUG : print(f"transaction data : {transaction_data}")
+
+            return JsonResponse({'success': True, 'message': f'request to {url} SUCCEED', 'redirect' : redirect_url})
+        else : 
+            return JsonResponse({'success': False, 'message': f'request to {url} FAILED with status code {response.status_code}'})
+
+    else:
+        allowed_methods = ['POST']
+        return HttpResponseNotAllowed(allowed_methods)
