a10d86fea56a227277449432f80b26db514c57b9
Romain Planchon <romain.planchon@telecom-paris.fr>
2024-02-27 14:22:30 +0100

    v2 transaction with adaptative phone number

--- sitweb/src/shop/views.py
+++ sitweb/src/shop/views.py
@@ -28,7 +28,6 @@ def shop(request):
 
 @csrf_protect
 def order_capsule(request):
-    DEBUG = False
 
     if request.method == 'POST':
         
@@ -36,36 +35,40 @@ def order_capsule(request):
         url = "https://homologation.lydia-app.com/api/request/do.json"
 
         capsule_amount = request.POST.get('capsule_prix')  
-        account_num = request.POST.get('account_num')
+        account_num = request.POST.get('phone_number')
         capsule_name = request.POST.get('capsule_nom')
-
+        print(f'account num : {account_num}')
+        message = f"ACHAT DE LA CAPSULE : {capsule_name}"
+        
         data = {
             "amount": int(capsule_amount)/100, #vérifier ce truc
             "payment_method": "auto",
             "vendor_token": vendor_token,
-            "recipient": account_num, # account_num à récupérer dans l'utilisateur
+            "recipient": str(account_num), # account_num à récupérer dans l'utilisateur
             "currency": "EUR",
             "type": "phone",
-            
+            "message" : message,
         }
 
         response = requests.post(url, data=data)
         if response.status_code == 200 : 
             transaction_data = response.json()
             
-            # Créer une transation en cours 
-            current_user = request.user
-            capsule = Capsule.objects.get(nom=capsule_name)  # Remplacez 'nom_capsule' par le nom de la capsule réelle
-            new_transaction = Transaction.objects.create(request_id = transaction_data["request_id"],author=current_user, contenu=capsule)
-            
-            redirect_url = transaction_data['mobile_url']
-            
             if DEBUG : print(f"transaction data : {transaction_data}")
-
-            return JsonResponse({'success': True, 'message': f'request to {url} SUCCEED', 'redirect' : redirect_url})
+            
+            if transaction_data['error'] == '0' : 
+                # Créer une transation en cours 
+                current_user = request.user
+                capsule = Capsule.objects.get(nom=capsule_name)  # Remplacez 'nom_capsule' par le nom de la capsule réelle
+                new_transaction = Transaction.objects.create(request_id = transaction_data["request_id"],author=current_user, contenu=capsule)
+                
+                redirect_url = transaction_data['mobile_url']
+                
+                return JsonResponse({'success': True, 'message': f'request to {url} SUCCEED', 'redirect' : redirect_url})
+            else : 
+                return JsonResponse({'success': False, 'message': f'transaction failed with error {transaction_data["error"]} | message : {transaction_data["message"]}'})
         else : 
             return JsonResponse({'success': False, 'message': f'request to {url} FAILED with status code {response.status_code}'})
-            # what if that failes
 
     else:
         allowed_methods = ['POST']
